64169846a16b7dbea8dbd35237101fb20228c575
BILGE <S1910237026@students.fh-hagenberg.at>
2020-11-01 17:21:55 +0100

    Firebase is installed

--- app/src/main/java/com/example/logintestproject/MainActivity.kt
+++ app/src/main/java/com/example/logintestproject/MainActivity.kt
@@ -1,12 +1,35 @@
 package com.example.logintestproject
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.google.firebase.analytics.FirebaseAnalytics
 
 class MainActivity : AppCompatActivity() {
+
+    private var clickNum = 0
+    private lateinit var textView : TextView
+    private lateinit var analytics : FirebaseAnalytics
+
+    @SuppressLint("SetTextI18n", "InvalidAnalyticsName")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
+
+        textView = findViewById(R.id.myText)
+        analytics = FirebaseAnalytics.getInstance(this) // google anatytics for button
+
+        val upButton = findViewById<Button>(R.id.button)
+        upButton.setOnClickListener {
+            clickNum++;
+            textView.text = "Value is $clickNum"
+            analytics.logEvent("button clicked", null)
+        }
+
     }
+
+
+
 }
