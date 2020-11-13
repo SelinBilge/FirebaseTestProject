2991a50ba44f7a8c829705d2ab87ded98d16a113
BILGE <S1910237026@students.fh-hagenberg.at>
2020-11-13 20:42:50 +0100

    test Login

--- app/src/main/java/com/example/logintestproject/MainActivity.kt
+++ app/src/main/java/com/example/logintestproject/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.example.logintestproject
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.widget.Button
 import android.widget.TextView
 import com.google.firebase.analytics.FirebaseAnalytics
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+
+import com.google.android.gms.common.SignInButton
+
+import com.google.android.gms.common.api.ApiException
+
+import com.google.android.gms.tasks.OnCompleteListener
+
+import com.google.android.gms.tasks.Task
+
+import com.google.firebase.auth.AuthCredential
+
+import com.google.firebase.auth.AuthResult
+
+import com.google.firebase.auth.FirebaseAuth
+
+import com.google.firebase.auth.FirebaseUser
+
+import com.google.firebase.auth.GoogleAuthProvider
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,11 +42,15 @@ class MainActivity : AppCompatActivity() {
     private lateinit var textView : TextView
     private lateinit var analytics : FirebaseAnalytics
 
+
+    private lateinit var mAuth: FirebaseAuth
+
     @SuppressLint("SetTextI18n", "InvalidAnalyticsName")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        /*
         textView = findViewById(R.id.myText)
         analytics = FirebaseAnalytics.getInstance(this) // google anatytics for button
 
@@ -27,6 +60,30 @@ class MainActivity : AppCompatActivity() {
             textView.text = "Value is $clickNum"
             analytics.logEvent("button clicked", null)
         }
+
+         */
+
+
+        mAuth = FirebaseAuth.getInstance()
+
+        val user = mAuth.currentUser
+
+        Handler().postDelayed({
+
+            if (user != null){ // is already logged in
+                val dashboardIntent = Intent(this, DashboardActivity::class.java)
+                startActivity(dashboardIntent)
+                finish()
+
+            } else { // user authenticate with google
+                val signInIntent = Intent(this, SignInActivity::class.java)
+                startActivity(signInIntent)
+                finish()
+            }
+
+        },2000)
+
+
 
     }
 
